5766e53bc8e95e611828dbf7fcf73d0ad484c46c
GhostDogGames <dorg50@gmail.com>
2023-03-11 11:39:26 +0200

    account fragment ui set

--- app/src/main/java/il/ghostdog/drawingapp/AccountFragment.kt
+++ app/src/main/java/il/ghostdog/drawingapp/AccountFragment.kt
@@ -5,10 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.findFragment
 
 class AccountFragment : Fragment(R.layout.fragment_account) {
+    private lateinit var photoMakerFragment: PhotoMakerFragment
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        photoMakerFragment = childFragmentManager.findFragmentById(R.id.my_fragment) as PhotoMakerFragment
     }
 }
--- app/src/main/java/il/ghostdog/drawingapp/PhotoMakerFragment.kt
+++ app/src/main/java/il/ghostdog/drawingapp/PhotoMakerFragment.kt
@@ -1,19 +1,31 @@
 package il.ghostdog.drawingapp
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.Color
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.Toast
+import android.widget.*
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.github.dhaval2404.colorpicker.ColorPickerDialog
 import com.github.dhaval2404.colorpicker.model.ColorShape
 
 
 class PhotoMakerFragment : Fragment(R.layout.fragment_photo_maker) {
+
     private lateinit var drawingView: DrawingView
+    private lateinit var mflDrawingView: FrameLayout
+    private lateinit var mBtnColor: ImageButton
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -21,14 +33,80 @@ class PhotoMakerFragment : Fragment(R.layout.fragment_photo_maker) {
         drawingView.setSizeForBrush(6.toFloat())
         drawingView.canDraw = true
 
+        mflDrawingView = view.findViewById(R.id.flDrawingViewContainer)
+
         val btnBrushSize = view.findViewById<ImageButton>(R.id.ibBrush)
         btnBrushSize.setOnClickListener{ showBrushSizeChooserDialog() }
 
         val ibUndo: ImageButton = view.findViewById(R.id.ibUndo)
         ibUndo.setOnClickListener { drawingView.onClickUndo() }
 
-        val btnColor: ImageButton = view.findViewById(R.id.ibColor)
-        btnColor.setOnClickListener { showColorPickerDialog() }
+        mBtnColor = view.findViewById(R.id.ibColor)
+        mBtnColor.setOnClickListener { showColorPickerDialog() }
+
+        val btnUploadPhoto : Button = view.findViewById(R.id.btnUploadPhoto)
+        btnUploadPhoto.setOnClickListener{ uploadPhoto()}
+    }
+
+    private fun uploadPhoto() {
+        requestStoragePermission()
+    }
+    private fun requestStoragePermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(
+                activity!!,
+                Manifest.permission.READ_EXTERNAL_STORAGE)){
+            showRationalDialog("Permission denied - access storage"
+                ,"Can not access storage of device for functions of the device for the application" )
+        }else{
+            requestPermission.launch(arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ))
+        }
+    }
+    private val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+                permissions ->
+            permissions.entries.forEach{
+                val permissionName = it.key
+                val isGranted =it.value
+
+                if(isGranted){
+                    if(permissionName == Manifest.permission.READ_EXTERNAL_STORAGE) {
+                        Toast.makeText(
+                            activity!!,
+                            "Permission granted to read files",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        val pickIntent = Intent(
+                            Intent.ACTION_PICK,
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                        )
+                        openGalleryLauncher.launch(pickIntent)
+                    }
+                }else{
+                    if(permissionName == Manifest.permission.READ_EXTERNAL_STORAGE){
+                        Toast.makeText(activity!!, "Permission denied", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    private val openGalleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+                result ->
+            if (result.resultCode == AppCompatActivity.RESULT_OK && result.data != null){
+                val imageBackground: ImageView = mflDrawingView.findViewById(R.id.ivBackground)
+                imageBackground.setImageURI(result.data?.data)
+            }
+        }
+    private fun showRationalDialog(title: String, message: String){
+        val builder: AlertDialog.Builder = AlertDialog.Builder(activity!!)
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.setPositiveButton("Cancel"){ dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.create().show()
     }
 
     private fun showColorPickerDialog(){
@@ -39,6 +117,7 @@ class PhotoMakerFragment : Fragment(R.layout.fragment_photo_maker) {
             .setDefaultColor(Color.BLACK)
             .setColorListener { color, colorHex ->
                 drawingView.setColor(colorHex)
+                mBtnColor.setBackgroundColor(color)
             }
             .show()
     }
@@ -74,5 +153,19 @@ class PhotoMakerFragment : Fragment(R.layout.fragment_photo_maker) {
         }
 
         brushDialog.show()
+    }
+    fun getBitmapFromView() : Bitmap {
+        val view = mflDrawingView
+        val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+        if(bgDrawable != null){
+            bgDrawable.draw(canvas)
+        }else{
+            canvas.drawColor(Color.WHITE)
+        }
+        view.draw(canvas)
+
+        return returnedBitmap
     }
 }
