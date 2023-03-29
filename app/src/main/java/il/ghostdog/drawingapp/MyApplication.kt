501e66f03122e149f7dcbd9be216a965020cb969
GhostDogGames <dorg50@gmail.com>
2023-03-30 00:50:41 +0300

    force light mode

---
+++ app/src/main/java/il/ghostdog/drawingapp/MyApplication.kt
@@ -0,0 +1,11 @@
+package il.ghostdog.drawingapp
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
